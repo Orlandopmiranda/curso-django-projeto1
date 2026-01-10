923a5091646375a3908dd96e446b02e7591620dc
Orlando Pereira de Miranda <104093026+Orlandopmiranda@users.noreply.github.com>
2026-01-09 22:06:00 -0300

    STATIC_URL e STATICFILES_DIRS no Django

--- base_templates/projeto/settings.py
+++ base_templates/projeto/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'recipes/static/'
+STATIC_URL = '/static/'
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATICFILES_DIRS = [
+    BASE_DIR / "base_static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
